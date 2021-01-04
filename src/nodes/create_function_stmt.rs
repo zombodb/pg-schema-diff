8ea28d86ddbab2b1b7a7da40227e23065de2ea34
Eric B. Ridge <eebbrr@gmail.com>
2021-01-03 20:14:14 -0700

    cleanup

--- src/nodes/create_function_stmt.rs
+++ src/nodes/create_function_stmt.rs
@@ -5,46 +5,6 @@ use postgres_parser::nodes::CreateFunctionStmt;
 use postgres_parser::sys::FunctionParameterMode::FUNC_PARAM_TABLE;
 use postgres_parser::Node;
 
-impl Diff for CreateFunctionStmt {
-    fn alter(&self, other: &Node) -> Option<String> {
-        if let Node::CreateFunctionStmt(other) = other {
-            let mut alter = String::new();
-            alter.push_str(&other.drop());
-            alter.push('\n');
-            alter.push_str(&self.sql());
-            Some(alter)
-        } else {
-            None
-        }
-    }
-
-    fn drop(&self) -> String {
-        let mut drop = String::new();
-        drop.push_str("DROP FUNCTION ");
-        drop.push_str(&make_name(&self.funcname).expect("no 'funcname' for CreateFunctionStmt"));
-        drop.push('(');
-        drop.push_str(
-            &self
-                .parameters
-                .as_ref()
-                .unwrap_or(&EMPTY_NODE_VEC)
-                .iter()
-                .filter(|p| {
-                    matches!(p,
-                    Node::FunctionParameter(param) if param.mode != FUNC_PARAM_TABLE)
-                })
-                .sql(),
-        );
-        drop.push(')');
-        drop.push_str(";");
-        drop
-    }
-
-    fn name(&self, _: &str) -> String {
-        make_name(&self.funcname).expect("unable to make name for CreateFunctionStatement")
-    }
-}
-
 impl Sql for CreateFunctionStmt {
     fn sql(&self) -> String {
         let mut returns_table = false;
@@ -98,8 +58,48 @@ impl Sql for CreateFunctionStmt {
             sql.push_str(&self.returnType.sql_prefix(" RETURNS "));
         }
 
-        sql.push_str(&self.options.sql(" "));
+        sql.push_str(&self.options.sql_prefix(" ", " "));
 
         sql
+    }
+}
+
+impl Diff for CreateFunctionStmt {
+    fn alter(&self, other: &Node) -> Option<String> {
+        if let Node::CreateFunctionStmt(other) = other {
+            let mut alter = String::new();
+            alter.push_str(&other.drop());
+            alter.push('\n');
+            alter.push_str(&self.sql());
+            Some(alter)
+        } else {
+            None
+        }
+    }
+
+    fn drop(&self) -> String {
+        let mut drop = String::new();
+        drop.push_str("DROP FUNCTION ");
+        drop.push_str(&make_name(&self.funcname).expect("no 'funcname' for CreateFunctionStmt"));
+        drop.push('(');
+        drop.push_str(
+            &self
+                .parameters
+                .as_ref()
+                .unwrap_or(&EMPTY_NODE_VEC)
+                .iter()
+                .filter(|p| {
+                    matches!(p,
+                    Node::FunctionParameter(param) if param.mode != FUNC_PARAM_TABLE)
+                })
+                .sql(),
+        );
+        drop.push(')');
+
+        drop
+    }
+
+    fn name(&self, _: &str) -> String {
+        make_name(&self.funcname).expect("unable to make name for CreateFunctionStatement")
     }
 }
